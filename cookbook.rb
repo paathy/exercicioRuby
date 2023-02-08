c5b65830ec4bbac87fa344773ce45c0a695ad8bd
paathy <pathy.limaap@gmail.com>
2023-02-08 02:04:19 -0300

    projeto final cookbook commit

--- cookbook.rb
+++ cookbook.rb
@@ -1,11 +1,17 @@
+INSERIR_RECEITA = 1
+VISUALIZAR_RECEITAS = 2
+#BUSCAR_RECEITAS = 3
+SAIR = 3
+
 def bem_vindo
     puts "Bem-vindo ao Cookbook, sua rede social de receitas"
 end
 
 def menu()
-    puts "[1] Cadastrar uma receita"
-    puts "[2] Ver todas as receitas"
-    puts "[3] Sair"
+    puts "[#{INSERIR_RECEITA}] Cadastrar uma receita"
+    puts "[#{VISUALIZAR_RECEITAS}] Ver todas as receitas"
+    #puts "[#{BUSCAR_RECEITAS}] Buscar receitas"
+    puts "[#{SAIR}] Sair"
 
     print "Escolha uma opção: " 
     return gets.to_i()
@@ -22,22 +28,29 @@ def inserir_receita()
     return{ nome: nome, tipo: tipo }
 end
 
-def imprimir_receitas(r)
+def imprimir_receitas(receitas)
     puts "========= Receitas Cadastradas ========="
-    r.each do |receita|
+    receitas.each do |receita|
     puts "#{receita[:nome]} - #{receita[:tipo]}"
     end 
     puts
+    if receitas.empty?
+        puts "Nenhuma receita cadastrada."
+        puts
+    end
+    
 end
 bem_vindo()
 receitas = []
 opcao = menu()
 
-while(opcao != 3) do
-    if (opcao == 1)
+loop do
+    if (opcao == INSERIR_RECEITA)
     receitas << inserir_receita()
-    elsif(opcao == 2)
+    elsif(opcao == VISUALIZAR_RECEITAS)
     imprimir_receitas(receitas)
+    elsif(opcao == SAIR)
+        break
     else
        puts "Opção inválida"
     end
